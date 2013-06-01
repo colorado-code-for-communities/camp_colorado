f1f411b056b122d295aa8cb8d405a7071d0f8813
sgrif <sean@thoughtbot.com>
2013-06-01 11:46:40 -0600

    Create Activities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130601164033) do
+ActiveRecord::Schema.define(:version => 20130601171157) do
+
+  create_table "activities", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "amenities", :force => true do |t|
     t.string   "name"
