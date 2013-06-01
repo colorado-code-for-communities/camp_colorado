32d8bda71ae98a2855703b9712642280315e2c7d
sgrif <sean@thoughtbot.com>
2013-06-01 09:10:35 -0600

    Add validations to campsite

--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,3 +1,8 @@
 class Campsite < ActiveRecord::Base
   attr_accessible :address, :close_date, :latitude, :longitude, :name, :open_date, :phone_number, :reservation_url, :website
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :latitude, presence: true, numericality: true
+  validates :longitude, presence: true, numericality: true
 end
--- spec/models/campsite_spec.rb
+++ spec/models/campsite_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Campsite do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:latitude) }
+    it { should validate_numericality_of(:latitude) }
+    it { should validate_presence_of(:longitude) }
+    it { should validate_numericality_of(:longitude) }
+  end
 end
