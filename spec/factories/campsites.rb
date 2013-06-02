acbe5b3e080a1d07f9a13e3b1a15a37e2f9a82d0
sgrif <sean@thoughtbot.com>
2013-06-01 19:18:15 -0600

    Fix *somebody* breaking all the specs

--- spec/factories/campsites.rb
+++ spec/factories/campsites.rb
@@ -3,7 +3,6 @@
 FactoryGirl.define do
   factory :campsite do
     sequence(:name) { |n| "Campsite #{n}" }
-    address "MyString"
     latitude 1.5
     longitude 1.5
     open_date "2013-06-01"
--- spec/models/campsite_spec.rb
+++ spec/models/campsite_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Campsite do
   describe 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:address) }
     it { should validate_presence_of(:latitude) }
     it { should validate_numericality_of(:latitude) }
     it { should validate_presence_of(:longitude) }
