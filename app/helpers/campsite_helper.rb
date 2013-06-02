55802b0a48ba1837a9785c62204fbb61cb811649
sgrif <sean@thoughtbot.com>
2013-06-01 19:58:37 -0600

    Display google map

--- app/helpers/campsite_helper.rb
+++ app/helpers/campsite_helper.rb
@@ -2,4 +2,13 @@ module CampsiteHelper
   def campsite_overview(campsite)
     (campsite.amenities + campsite.activities).take(5).map(&:name)
   end
+
+  def static_map_url(campsite)
+    "http://maps.googleapis.com/maps/api/staticmap" <<
+    "?center=#{campsite.latitude},#{campsite.longitude}" <<
+    "&zoom=11&size=304x190" <<
+    "&markers=#{campsite.latitude},#{campsite.longitude}" <<
+    "&key=#{ENV['GOOGLE_API_KEY']}" <<
+    "&sensor=false"
+  end
 end
